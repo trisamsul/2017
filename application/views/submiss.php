a6e23884ae4c338976d34fd1fbcc0d8d410c401b
trisamsul <trisamsul@gmail.com>
2016-11-17 16:40:06 +0700

    submiss

--- application/views/submiss.php
+++ application/views/submiss.php
@@ -14,7 +14,7 @@
 				    <li>All review processes are blind peer-reviews.</li>
 				    <li>Non-presented papers will be pulled from submission to IEEE Xplore.</li>
 				    <li>Full paper must be submitted through <a href="https://edas.info/N23104" title="" target="_blank"class="link1"><b>EDAS</b></a> System.</li>
-				    <li>Please contact <a href="mailto:info@icsitech.org" class="email">info@icsitech.org</a> and/or <a href="mailto:lala@upi.edu" class="email">lala@upi.edu</a>, <a href="mailto:andri.pranolo.id@ieee.org" class="email">andri.pranolo.id@ieee.org</a> with the questions.</li>
+				    <li>Please contact <a href="mailto:info@icsitech.org" class="email">info@icsitech.org</a> and/or <a href="mailto:lala.s.riza@upi.edu" class="email">lala.s.riza@upi.edu</a>, <a href="mailto:andri.pranolo.id@ieee.org" class="email">andri.pranolo.id@ieee.org</a> with the questions.</li>
 				</ol>
 			<li><b>FINAL SUBMISSION</b> for <b>ACCEPTED PAPERS,</b> please do the following steps:</li>
 				<ol>
@@ -22,7 +22,7 @@
 					<li>Validted and Converted your paper in <b>PDF format</b> to <a href="http://www.pdf-express.org/" title="" target="_blank"class="link1"><b>IEEE Xplore-compatible PDFs through PDF Express</b></a> with <b>conference ID 40947XP</b></li>
 					<li>Submit your papers <b>in IEEE Xplore-compatible PDFs</b> to ICSITech 2017 through <b>EDAS</b></li>
 					<li>Fill out electronic IEEE Copyright Form (eCF) through <b>EDAS</b></li>
-					<li>Finally, you are requested to full fill registration from through <a href="http://icsitech.org/read/registration" title="" target="_blank"class="link1">here</a>. Check your name spelling, because it will be used for the invitation letter/conference certificate.</li>
+					<li>Finally, you are requested to full fill registration form through <a href="<?php echo site_url('/Registration'); ?>" title="" target="_blank"class="link1">here</a>. Check your name spelling, because it will be used for the invitation letter/conference certificate.</li>
 				</ol>
 		</ol>
 	</div>
